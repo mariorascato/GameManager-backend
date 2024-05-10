81fa65357933a038bb8ecc66b9b13175ee4f7199
mariolino21714@gmail.com <mariolino21714@gmail.com>
2024-05-10 10:43:44 +0200

    iniziato a sviluppare il rest Controller di Game

--- src/main/java/it/unimol/mobile/gamemanager/repository/GameRepository.java
+++ src/main/java/it/unimol/mobile/gamemanager/repository/GameRepository.java
@@ -4,7 +4,14 @@ import it.unimol.mobile.gamemanager.model.game.Game;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 
 public interface GameRepository extends MongoRepository<Game,String> {
+    public Optional<Game> getGameByNome(String nome);
+    public Optional<List<Game>> getGameBySviluppatore(String sviluppatore);
+
+
 }
--- src/main/java/it/unimol/mobile/gamemanager/service/PlayerService.java
+++ src/main/java/it/unimol/mobile/gamemanager/service/PlayerService.java
@@ -1,5 +1,6 @@
 package it.unimol.mobile.gamemanager.service;
 
+import it.unimol.mobile.gamemanager.model.game.Game;
 import it.unimol.mobile.gamemanager.model.player.Player;
 import it.unimol.mobile.gamemanager.repository.PlayerRepository;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class PlayerService {
+
     private final PlayerRepository playerRepository;
     public ResponseEntity<Player> addPlayer(Player player ){
         if(playerRepository.findByEmail(player.getEmail()).isPresent()){
@@ -21,6 +23,14 @@ public class PlayerService {
         playerRepository.save(player);
 
         return ResponseEntity.status(HttpStatus.OK).body(player);
+    }
+    public ResponseEntity<Player> findPlayerByEmail(String email){
+        if(playerRepository.findByEmail(email).isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(playerRepository.findByEmail(email).get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
     }
     public ResponseEntity<List<Player>> getAllPlayers(){
         if(this.playerRepository.findAll().isEmpty()){
@@ -48,6 +58,27 @@ public class PlayerService {
             return ResponseEntity.status(HttpStatus.OK).body(playerToUpdate);
         }
     }
+    public ResponseEntity<Player> deletePlayer(String id){
+        if(this.playerRepository.findById(id).isEmpty()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        this.playerRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    public ResponseEntity<List<Game>> getAllGiochiPosseduti(String id){
+        if(this.playerRepository.findById(id).isEmpty()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(this.playerRepository.findById(id).get().getGiochiPosseduti());
+    }
+    public ResponseEntity<List<Game>> getAllGiochiPreferiti(String id){
+        if(this.playerRepository.findById(id).isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(this.playerRepository.findById(id).get().getGiochiPreferiti());
+    }
+
 
 
 }
